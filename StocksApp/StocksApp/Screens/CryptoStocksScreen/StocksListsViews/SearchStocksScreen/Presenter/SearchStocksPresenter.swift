56f6cfae3606c43ab795ac5462713b1a346bf79e
arleymi <archerlv99@gmail.com>
2022-06-09 14:22:24 +0600

    review remarks done

--- StocksApp/StocksApp/Screens/CryptoStocksScreen/StocksListsViews/SearchStocksScreen/Presenter/SearchStocksPresenter.swift
+++ StocksApp/StocksApp/Screens/CryptoStocksScreen/StocksListsViews/SearchStocksScreen/Presenter/SearchStocksPresenter.swift
@@ -85,8 +85,7 @@ final class SearchStocksPresenter: SearchStocksPresenterProtocol {
 extension SearchStocksPresenter: FavoritesUpdateServiceProtocol {
     
     func setFavoriteNotification(notification: Notification) {
-        guard let id = notification.stockID else { return }
-        guard let index = stocks.firstIndex(where: { $0.id == id }) else { return }
+        guard let id = notification.stockID, let index = stocks.firstIndex(where: { $0.id == id }) else { return }
         let indexPath = IndexPath(row: index, section: 0)
 
         view?.updateCell(for: indexPath)
--- StocksApp/StocksApp/Screens/CryptoStocksScreen/StocksListsViews/SearchStocksScreen/View/SearchStocksViewController.swift
+++ StocksApp/StocksApp/Screens/CryptoStocksScreen/StocksListsViews/SearchStocksScreen/View/SearchStocksViewController.swift
@@ -10,63 +10,75 @@ import UIKit
 final class SearchStocksViewController: UIViewController {
     
     private lazy var tableView: UITableView = {
+        
         let tableView = UITableView()
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.register(StockCell.self, forCellReuseIdentifier: StockCell.typeName)
         tableView.separatorStyle = .none
         tableView.backgroundColor = .white
+        
         return tableView
     }()
     
     private lazy var searchViewContainer: UIView = {
+        
         let view = UIView()
         view.backgroundColor = .white
         view.translatesAutoresizingMaskIntoConstraints = false
         view.layer.borderWidth = 2
         view.layer.borderColor = UIColor.black.cgColor
         view.layer.cornerRadius = 25
+        
         return view
     }()
     
     private lazy var titleViewContainer: UIView = {
+        
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
         view.backgroundColor = .white
+        
         return view
     }()
     
     private lazy var titleOfList: UILabel = {
+        
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Crypto"
         label.font = UIFont(name: "Montserrat-Bold", size: 18)
-        label.font = .boldSystemFont(ofSize: 18)
+        
         return label
     }()
     
     private lazy var cancelButton: UIButton = {
+        
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
         button.setImage(UIImage(named: "cancelButton"), for: .normal)
         button.addTarget(self, action: #selector(cancelButtonTapped), for: .touchUpInside)
+        
         return button
     }()
     
     private lazy var clearButton: UIButton = {
+        
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
         button.setImage(UIImage(named: "clearButton"), for: .normal)
         button.addTarget(self, action: #selector(clearButtonTapped), for: .touchUpInside)
+        
         return button
     }()
     
     private lazy var searchTextField: UITextField = {
+        
         let textField = UITextField()
         textField.translatesAutoresizingMaskIntoConstraints = false
         textField.placeholder = "Type cryptocurrency"
         textField.backgroundColor = .white
         textField.returnKeyType = .search
         textField.addTarget(self, action: #selector(textFieldDidChanged), for: .editingChanged)
+        
         return textField
     }()
 
@@ -124,32 +136,41 @@ final class SearchStocksViewController: UIViewController {
     
     private func setUpSubViewsConstraints() {
         
-        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
-        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
-        tableView.topAnchor.constraint(equalTo: titleViewContainer.bottomAnchor).isActive = true
-        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-        
-        searchViewContainer.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 7).isActive = true
-        searchViewContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
-        searchViewContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
-        searchViewContainer.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        
-        titleViewContainer.topAnchor.constraint(equalTo: searchViewContainer.bottomAnchor).isActive = true
-        titleViewContainer.heightAnchor.constraint(equalToConstant: 30).isActive = true
-        titleViewContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
-        titleViewContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
-        
-        titleOfList.leadingAnchor.constraint(equalTo: titleViewContainer.leadingAnchor).isActive = true
-        titleOfList.topAnchor.constraint(equalTo: titleViewContainer.topAnchor, constant: 9).isActive = true
-        
-        cancelButton.leadingAnchor.constraint(equalTo: searchViewContainer.leadingAnchor, constant: 16).isActive = true
-        cancelButton.topAnchor.constraint(equalTo: searchViewContainer.topAnchor, constant: 13).isActive = true
-        
-        clearButton.trailingAnchor.constraint(equalTo: searchViewContainer.trailingAnchor, constant: -16).isActive = true
-        clearButton.topAnchor.constraint(equalTo: searchViewContainer.topAnchor, constant: 13).isActive = true
-        
-        searchTextField.leadingAnchor.constraint(equalTo: cancelButton.trailingAnchor, constant: 16).isActive = true
-        searchTextField.topAnchor.constraint(equalTo: searchViewContainer.topAnchor, constant: 13).isActive = true
+        NSLayoutConstraint.activate([
+            
+            /// TableView
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            tableView.topAnchor.constraint(equalTo: titleViewContainer.bottomAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            /// searchContainer
+            searchViewContainer.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 7),
+            searchViewContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            searchViewContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            searchViewContainer.heightAnchor.constraint(equalToConstant: 50),
+            
+            /// titleContainer
+            titleViewContainer.topAnchor.constraint(equalTo: searchViewContainer.bottomAnchor),
+            titleViewContainer.heightAnchor.constraint(equalToConstant: 30),
+            titleViewContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            titleViewContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            
+            /// titleOfList
+            titleOfList.leadingAnchor.constraint(equalTo: titleViewContainer.leadingAnchor),
+            titleOfList.topAnchor.constraint(equalTo: titleViewContainer.topAnchor, constant: 9),
+            
+            cancelButton.leadingAnchor.constraint(equalTo: searchViewContainer.leadingAnchor, constant: 16),
+            cancelButton.topAnchor.constraint(equalTo: searchViewContainer.topAnchor, constant: 13),
+            
+            ///clearButton
+            clearButton.trailingAnchor.constraint(equalTo: searchViewContainer.trailingAnchor, constant: -16),
+            clearButton.topAnchor.constraint(equalTo: searchViewContainer.topAnchor, constant: 13),
+            
+            ///searchTextField
+            searchTextField.leadingAnchor.constraint(equalTo: cancelButton.trailingAnchor, constant: 16),
+            searchTextField.topAnchor.constraint(equalTo: searchViewContainer.topAnchor, constant: 13)
+        ])
     }
     
     @objc private func clearButtonTapped() {
@@ -165,9 +186,7 @@ final class SearchStocksViewController: UIViewController {
     @objc private func textFieldDidChanged(sender: UITextField) {
         presenter.searchStock(searching: sender.text!)
     }
-
 }
-
 
 extension SearchStocksViewController: UITableViewDataSource {
     
